e9b64112ef5933471a00cc3b0e69951b06d49ecf
Tony Ferrell <tony@codeawhile.com>
2014-08-05 01:21:00 -0700

    Rewrite setup using 'functions'

--- setup.cmd
+++ setup.cmd
@@ -1,51 +1,100 @@
-set _tempvbs=%LocalAppData%\getadmin.vbs
-
-setlocal
+@setlocal enabledelayedexpansion enableextensions
 @echo off
+
+set _tempvbs=%LocalAppData%\getadmin.vbs
+set swapDir=".vim\swap"
 
 :: Check for admin permissions
 Net Session >nul 2>&1
+
 :: If error flag set, we do not have admin.
-If ERRORLEVEL 1 (
-   Echo Requesting administrative privileges...
-   Goto sub_elevate
-) Else ( Goto sub_main )
+if ERRORLEVEL 1 (
+  echo Requesting administrative privileges and restarting...
+  goto rerun_script_elevated
+)
 
-:sub_elevate
-   echo Set objShell = CreateObject^("Shell.Application"^) > %_tempvbs%
-   echo objShell.ShellExecute "%~f0", "", "", "runas", 1 >> %_tempvbs%
-   cscript "%_tempvbs%" //nologo
-   Exit /B
+:: Script should be in the root of the dot-files directory - wherever that is.
+call :directory_name_from_path fileRoot %0
 
-:sub_main
-   if exist "%_tempvbs%" ( Del "%_tempvbs%" )
+pushd %fileRoot%
 
-   :: Script should be in the root of the dot-files directory - wherever that is.
-   set fileRoot=%~dp0
+if not exist %swapDir% (
+  mkdir %swapDir%
+)
 
-   pushd %fileRoot%
+if not exist ".vim\bundle\Vundle.vim" (
+  echo "Cloning Vundle into .vim/bundle/Vundle.vim"
+  git clone git@github.com:gmarik/Vundle.vim.git .vim\bundle\Vundle.vim
+)
 
-   if not exist ".vim\bundle\Vundle.vim" (
-      echo "Downloading Vundle (vim package manager) into .vim\bundle\Vundle.vim"
-      git clone git@github.com:gmarik/Vundle.vim.git .vim\bundle\Vundle.vim
+call :link_if_missing ".vimrc" %USERPROFILE% %fileRoot%
+call :link_if_missing ".bash_profile" %USERPROFILE% %fileRoot%
+call :link_if_missing ".bashrc" %USERPROFILE% %fileRoot%
+call :link_if_missing ".vim" %USERPROFILE% %fileRoot%
+
+echo Linking finished. Press any key to install Vundle plugins
+@pause
+
+:: Install Vundle Plugins
+vim +PluginInstall +qall
+
+pause
+
+popd
+
+@goto :eof
+
+:: Function Declarations - commented because cmd is cryptic, and I hope they can be reused.
+
+:: Create an elevated command prompt using the script host and run this script.
+:rerun_script_elevated (
+  echo Set objShell = CreateObject^("Shell.Application"^) > %_tempvbs%
+  echo objShell.ShellExecute "%~f0", "", "", "runas", 1 >> %_tempvbs%
+  cscript "%_tempvbs%" //nologo
+
+  ::if exist "%_tempvbs%" ( del "%_tempvbs%" )
+  exit /B
+)
+
+:: Given a path, extract the directory.
+:: Example usage:
+::    call :directory_name_from_path  result "C:\Directory1\Dir2\myfile.txt"
+::    echo %result%  ::Echos C:\Directory1\Dir2\
+:directory_name_from_path <resultVar> <pathVar>
+(
+   set "%~1=%~dp2"
+   exit /b
+)
+
+:: Gets the output of a command, and stores it in a variable named <resultVar>
+:: Example usage:
+::    call :get_command_output result "where git"
+::    echo %result%  ::Echos the location of your git command.
+:get_command_output <resultVar> <command>
+(
+   FOR /F "delims=" %%i IN ('%~2') DO @set "%~1=%%i"
+   exit /b
+)
+
+:link_if_missing <file> <linkInto> <linkFrom>
+(
+  set! destination="%~2\%~1"
+  set! source="%~3%~1"
+  if not exist !destination! (
+    if exist "!source!\*" (
+      mklink /D !destination! !source!
+      exit /b
+    ) else (
+      if exist "!source!" (
+        mklink !destination! !source!
+        exit /b
+      )
+    )
+
+    echo "Unable to link !source!. File does not exist."
+    exit /b
    )
+)
 
-   if not exist ".vim\swap\" (
-      mkdir .vim\swap
-   )
-
-   if not exist "%USERPROFILE%\.vim" (mklink /D %USERPROFILE%\.vim %fileRoot%.vim)
-   if not exist "%USERPROFILE%\.vimrc" (mklink %USERPROFILE%\.vimrc %fileRoot%.vimrc)
-   if not exist "%USERPROFILE%\.bashrc" (mklink %USERPROFILE%\.bashrc %fileRoot%.bashrc)
-   if not exist "%USERPROFILE%\.bash_profile" (mklink %USERPROFILE%\.bash_profile %fileRoot%.bash_profile)
-
-   echo Continue to install Vundle plugins
-   pause
-   :: Install Vundle Plugins
-   vim +PluginInstall +qall
-
-   echo Install completed
-   pause
-   popd
-
-endlocal
+:eof
+@endlocal
